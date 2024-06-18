5329c52b16955e5516948673b701ed1b87a10180
Hezavehir <hezavehir@gmail.com>
2024-06-18 21:11:22 +0330

    Developing input and output functions.

--- SHA1.cpp
+++ SHA1.cpp
@@ -1,6 +1,44 @@
+#include <string>
 #include <iostream>
 using namespace std;
+
+unsigned int h[4];
+string input_data;
+
+void getInputHashParameters()
+{
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Input the H[" << i << "]: ";
+        cin >> h[i];
+    }
+}
+void getInputData()
+{
+    cout << "Input Data:\n";
+    getline(cin,input_data);
+    // I wrote this twice because the inputted endline of the previous outputs will be read by the first getline().
+    getline(cin,input_data);
+}
+void getInputs()
+{
+    getInputHashParameters();
+    getInputData();
+}
+
+void printOutputs()
+{
+    cout << "\nDecimal representation of the hash:\n";
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << h[i] << ' ';
+    }
+    cout<<'\n';
+}
+
 int main()
 {
+    getInputs();
+    printOutputs();
     return 0;
 }
